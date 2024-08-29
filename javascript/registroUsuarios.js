a2411c38e8073f5c8e4304289e7e5f017c371acd
KevinGarcia2007 <kevingamercamiloop>
2024-08-28 22:09:42 -0500

    arreglo sintaxis en registrar usuarios

--- javascript/registroUsuarios.js
+++ javascript/registroUsuarios.js
@@ -1,12 +1,9 @@
-    // Escuchar el cambio en el campo de selección de rol
-    document.getElementById('rol').addEventListener('change', function() {
-        var cursosDisponibles = document.getElementById('cursosDisponibles');
-        // Mostrar el campo de grados disponibles solo si el rol es docente
-        if (this.value === 'docente') {
-          cursosDisponibles.style.display = 'block';
-
-        } else {
-          cursosDisponibles.style.display = 'none';
-
-        }
-      });
+document.getElementById('rol').addEventListener('change', function() {
+  var cursosDisponibles = document.getElementById('cursosDisponibles');
+  // Mostrar el campo de grados disponibles solo si el rol es docente
+  if (this.value === 'docente') {
+    cursosDisponibles.style.display = 'block';
+  } else {
+    cursosDisponibles.style.display = 'none';
+  }
+});
