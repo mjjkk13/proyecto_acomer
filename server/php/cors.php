f2bf56a7579842b46e5d640acf95f13ac195da51
mjjkk13 <marianajimenezv2006@gmail.com>
2025-06-07 11:48:02 -0500

    + cors

--- server/php/cors.php
+++ server/php/cors.php
@@ -10,7 +10,7 @@ $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 if (in_array($origin, $allowed_origins)) {
     header("Access-Control-Allow-Origin: $origin");
     header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, Cache-Control, X-Requested-With');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Max-Age: 86400'); // cache 1 día
 }
